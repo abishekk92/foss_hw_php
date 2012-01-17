89f52f386402982ebd1d5d50607779c2de96a047
Abishek Bhat <bhat.abishek@gmail.com>
2012-01-17 11:47:29 +0530

    Added documentation

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+ A bunch of scripts and apps which I wrote as a part of my university programme on Free and Open Source Software.
---
+++ dry/protected/models/ContactForm.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * ContactForm class.
+ * ContactForm is the data structure for keeping
+ * contact form data. It is used by the 'contact' action of 'SiteController'.
+ */
+class ContactForm extends CFormModel
+{
+	public $name;
+        public $id
+	public $email;
+        public $mobile;
+	public $designation;
+	public $academic;
+   	public $year;
+	public $branch;
+	public $verifyCode;
+
+	/**
+	 * Declares the validation rules.
+	 */
+	public function rules()
+	{
+		return array(
+			// name, email, subject and body are required
+			array('name, email,year, branch', 'required'),
+			// email has to be a valid email address
+			array('email', 'email'),
+			// verifyCode needs to be entered correctly
+			array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
+		);
+	}
+
+	/**
+	 * Declares customized attribute labels.
+	 * If not declared here, an attribute would have a label that is
+	 * the same as its name with the first letter in upper case.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'verifyCode'=>'Verification Code',
+		);
+	}
+}
